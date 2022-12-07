1b9c92cb8157ecb870ee3ab10c2451beb7ee8153
Hanyi Liu <hanyiliu0@gmail.com>
2022-12-06 20:04:40 -0800

    Initial Commit

---
+++ Linker/LinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkerApp.swift
+//  Linker
+//
+//  Created by Hanyi Liu on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
